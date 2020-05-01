dad18b0e226928a71eef70b0cc2caec1e7e05c80
Shubham401 <shubham26.bdps@gmail.com>
2020-05-01 16:01:34 +0530

    Signup-Signin

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+require('dotenv').config();
+
+//import routes
+const userRoutes = require('./routes/user');
+
+// app
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+    .then(response => { 
+        console.log('MongoDB Connected');
+    }).catch(err => {
+        console.log('Connection Failed'); 
+    })
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cookieParser());
+app.use(expressValidator());
+
+//routesMiddleware
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
