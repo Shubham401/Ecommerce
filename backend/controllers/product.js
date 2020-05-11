1dc75c00004d2ed25bbf2a07050ebb6e2931358b
Shubham401 <shubham26.bdps@gmail.com>
2020-05-11 18:13:35 +0530

    Product Validation

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -13,9 +13,29 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded.'
             });
         }
+        //check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required.'
+            });
+        }
+
+
         let product = new Product(fields);
 
+
+        //1kb = 1000
+        //1mb = 100000
+
         if (files.photo) {
+            //console.log("FILES PHOTO: ", files.photo);
+            if(files.photo.size > 100000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb.'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
