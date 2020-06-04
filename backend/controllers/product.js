69aa4a5de9054dd7133609b581a35a8f5da23556
Shubham401 <shubham26.bdps@gmail.com>
2020-06-04 16:54:55 +0530

    Product delete and update

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -67,3 +67,65 @@ exports.create = (req, res) => {
         });
    });
 };
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            "message": 'Product deleted successfully'
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+         if (err){
+             return res.status(400).json({
+                 error: 'Image could not be uploaded.'
+             });
+         }
+         //check for all fields
+         const {name, description, price, category, quantity, shipping} = fields
+ 
+         if (!name || !description || !price || !category || !quantity || !shipping) {
+             return res.status(400).json({
+                 error: 'All fields are required.'
+             });
+         }
+ 
+ 
+         let product = req.product;
+         product = _.extend(product, fields)
+ 
+ 
+         //1kb = 1000
+         //1mb = 100000
+ 
+         if (files.photo) {
+             //console.log("FILES PHOTO: ", files.photo);
+             if(files.photo.size > 100000) {
+                 return res.status(400).json({
+                     error: 'Image should be less than 1mb.'
+                 });
+             }
+             product.photo.data = fs.readFileSync(files.photo.path);
+             product.photo.contentType = files.photo.type;
+         }
+ 
+         product.save((err, result) => {
+             if (err) {
+                 return res.status(400).json({
+                     error: errorHandler(err)
+                 });
+             }
+             res.json(result);
+         });
+    });
+ };
