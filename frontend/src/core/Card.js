8b45dee52929a58bfd5520d91a1273b86b44ae0d
Shubham401 <shubham26.bdps@gmail.com>
2022-07-16 22:05:53 +0530

    Cart-CRUD with LocalStorage

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import { addItem, updateItem, removeItem } from './cartHelpers';
 
-const Card = ({ product, showViewProductButton = true }) => {
+const Card = ({
+    product,
+    showViewProductButton = true,
+    showAddToCartButton = true,
+    cartUpdate = false,
+    showRemoveProductButton = false,
+    setRun = f => f,
+    run = undefined
+}) => {
+    const [redirect, setRedirect] = useState(false);
+    const [count, setCount] = useState(product.count);
+
     const showViewButton = (showViewProductButton) => {
         return (
             showViewProductButton && (
@@ -14,9 +26,44 @@ const Card = ({ product, showViewProductButton = true }) => {
         );
     };
 
-    const showAddToCartButton = () => {
+    const addToCart = () => {
+        addItem(product, () => {
+            setRedirect(true);
+        })
+    }
+
+    const shouldRedirect = redirect => {
+        if (redirect) {
+            return <Redirect to="/cart" />
+        }
+    }
+
+    const showAddToCart = (showAddToCartButton) => {
         return (
-            <button className='btn btn-outline-warning mt-2 mb-2'>Add to cart</button>
+            showAddToCartButton && (
+                <button
+                    onClick={addToCart}
+                    className='btn btn-outline-warning mt-2 mb-2'
+                >
+                    Add to cart
+                </button>
+            )
+        );
+    };
+
+    const showRemoveProduct = showRemoveProductButton => {
+        return (
+            showRemoveProductButton && (
+                <button
+                    onClick={() => {
+                        removeItem(product._id)
+                        setRun(!run)
+                    }}
+                    className='btn btn-outline-danger mt-2 mb-2'
+                >
+                    Remove Product
+                </button>
+            )
         );
     };
 
@@ -28,10 +75,39 @@ const Card = ({ product, showViewProductButton = true }) => {
         );
     };
 
+    const handleChange = productId => event => {
+        setRun(!run);
+        setCount(event.target.value < 1 ? 1 : event.target.value)
+        if (event.target.value >= 1) {
+            updateItem(productId, event.target.value);
+        }
+    };
+
+    const showCartUpdateOptions = cartUpdate => {
+        return (
+            cartUpdate && (
+                <div>
+                    <div className='input-group mb-3'>
+                        <div className='input-group-prepend'>
+                            <span className='input-group-text'>Adjust Quantity</span>
+                        </div>
+                        <input
+                            type='number'
+                            className='form-control'
+                            value={count}
+                            onChange={handleChange(product._id)}
+                        />
+                    </div>
+                </div>
+            )
+        );
+    };
+
     return (
         <div className='card'>
             <div className='card-header name'>{product.name}</div>
             <div className='card-body'>
+                {shouldRedirect(redirect)}
                 <ShowImage item={product} url="product" />
                 <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
                 <p className='black-10'>${product.price}</p>
@@ -40,7 +116,9 @@ const Card = ({ product, showViewProductButton = true }) => {
                 {showStock(product.quantity)}
                 <br />
                 {showViewButton(showViewProductButton)}
-                {showAddToCartButton()}
+                {showAddToCart(showAddToCartButton)}
+                {showRemoveProduct(showRemoveProductButton)}
+                {showCartUpdateOptions(cartUpdate)}
             </div>
         </div>
     );
